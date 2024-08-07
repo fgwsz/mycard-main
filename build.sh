6c1b5117745646063f5f3b8b027c6fa03663ccb0
fgwsz <3191000458@qq.com>
2024-08-07 14:17:27 +0800

    clean

--- build.sh
+++ build.sh
@@ -16,14 +16,19 @@ sudo npm install -g cnpm --registry=https://registry.npmmirror.com
 # download mycard v3
 mycard_download_url="https://code.mycard.moe/mycard/mycard/-/archive/v3/mycard-v3.tar.gz"
 mycard_path="$root_path/mycard-v3.tar.gz"
-curl -C - -o "$mycard_path" "$mycard_download_url"
-cd "$root_path"
-mkdir mycard
-tar -zxvf mycard-v3.tar.gz -C mycard/
+if [ ! -d "$mycard_path" ]; then
+    curl -C - -o "$mycard_path" "$mycard_download_url"
+fi
+mycard_dir_path="$root_path/mycard"
+if [ ! -d "$mycard_dir_path" ]; then
+    mkdir -p "$mycard_dir_path"
+fi
+tar -zxvf "$mycard_path" -C "$mycard_dir_path/"
 # for mycard v3
 sudo apt update
 sudo apt -y install python3 build-essential git
 cd "$root_path/mycard/mycard-v3"
+sudo cnpm install --save electron@4.2.12
 sudo cnpm ci -f --openssl_fips=''
 sudo cp "$root_path/package.json" "$root_path/mycard/mycard-v3/package.json"
 sudo cnpm run dist --openssl_fips=''
