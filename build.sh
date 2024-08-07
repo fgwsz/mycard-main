727bd67c9d62a8ad54472f52d3aa53fdde43e2e4
fgwsz <3191000458@qq.com>
2024-08-07 15:49:00 +0800

    clean

--- build.sh
+++ build.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
-# install curl node npm python3 build-essential git libssl-dev
-sudo apt update
-sudo apt install curl
-sudo apt -y install python3 build-essential git
-sudo apt-get install libssl-dev
-# 下面的18指的是nodejs的大版本号,填入你需要的最新版版号即可
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo apt-get install nodejs
-# install cnpm
-sudo npm config set registry https://registry.npmmirror.com
-sudo npm install -g cnpm --registry=https://registry.npmmirror.com
-# download mycard v3
-mycard_download_url="https://code.mycard.moe/mycard/mycard/-/archive/v3/mycard-v3.tar.gz"
-mycard_path="$root_path/mycard-v3.tar.gz"
-if [ ! -e "$mycard_path" ]; then
-    curl -C - -o "$mycard_path" "$mycard_download_url"
+mycard_file_path="$root_path/mycard-v3.tar.gz"
+if [ ! -e "$mycard_file_path" ]; then
+    "$root_path/download.sh"
 fi
-mycard_dir_path="$root_path/mycard"
-if [ ! -e "$mycard_dir_path" ]; then
-    mkdir -p "$mycard_dir_path"
+mycard_root_path="$root_path/mycard"
+if [ ! -e "$mycard_root_path" ]; then
+    mkdir -p "$mycard_root_path"
+    tar -zxvf "$mycard_file_path" -C "$mycard_root_path/"
 fi
-tar -zxvf "$mycard_path" -C "$mycard_dir_path/"
-# for mycard v3
-sudo apt update
-sudo apt -y install python3 build-essential git
-cd "$root_path/mycard/mycard-v3"
+mycard_dir_path="$mycard_root_path/mycard-v3"
+cd "$mycard_dir_path"
 sudo cnpm install --save electron@4.2.12
 sudo cnpm ci -f --openssl_fips=''
-sudo cp "$root_path/package.json" "$root_path/mycard/mycard-v3/package.json"
+sudo cp "$root_path/package.json" "$mycard_dir_path/package.json"
 sudo cnpm run dist --openssl_fips=''
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
-"$root_path/mycard/mycard-v3/dist/MyCard-3.0.70.AppImage"
+mycard_path="$root_path/mycard/mycard-v3/dist/MyCard-3.0.70.AppImage"
+if [ ! -e "$mycard_path" ]; then
+    "$root_path/build.sh"
+fi
+"$mycard_path"
