8a1ec4e7343295a2c2d6a8ef9d0f3f890b468462
fgwsz <3191000458@qq.com>
2025-01-03 00:12:00 +0800

    change download option by curl

--- download.sh
+++ download.sh
@@ -5,12 +5,9 @@ mycard_download_url="https://code.moenext.com/mycard/mycard/-/archive/v3.0.71/my
 mycard_path="$root_path/mycard-v3.0.71.tar.gz"
 if [ ! -e "$mycard_path" ]; then
     while true; do
-        curl -C - -o "$mycard_path" "$mycard_download_url"
+        curl --http1.1 -C - --retry 999 --retry-delay 2 -o "$mycard_path" "$mycard_download_url"
         if [ $? -eq 0 ]; then
             break
-        fi
-        if [[ -e "$mycard_path" ]]; then
-            rm -rf "$mycard_path"
         fi
     done
 fi
